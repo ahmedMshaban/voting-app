841a2bb6b333764d7c405df0f7b1334cbac50313
ahmedMshaban <ahmed.ms.elias@gmail.com>
2022-03-12 09:17:11 +0300

    ProductList structure

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className="ui unstackable itmes">
+        Hello, friend! I am a basic React component.
+      </div>
+    );
+  }
+}
 
+ReactDOM.render(<ProductList />, document.getElementById("content"));
